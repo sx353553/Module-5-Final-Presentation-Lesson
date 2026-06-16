5e93c23b773b9448eaa43f86ae452d97958f2434
Raul Castillo <sx353553@yahoo.com>
2026-06-16 13:38:57 -0500

    update code

---
+++ index.js
@@ -0,0 +1,25 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
+
+async function main() {
+const users = await fetch("https://jsonplaceholder.typicode.com/users")
+const userData = await users.json();
+userListEl.innerHTML = userData.map((user) => userHTML(user)).join("");
+}
+
+main();
+
+function userHTML(user) {
+    return `<div class="user-card">
+        <div class="user">
+            <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="http://${user.website}" target="_blank">
+            ${user.website}
+            </a></p>
+        </div>
+        </div>
+    `;
+}
