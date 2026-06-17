73fec1e765e1445463f2dbabfad8ecc18716e4e6
sx353553 <sx353553@yahoo.com>
2026-06-16 19:00:28 -0500

    update code

--- user.js
+++ user.js
@@ -1,9 +1,20 @@
-
+const postListEl = document.querySelector(".posts-list");
 async function main() {
   const id = localStorage.getItem("id");
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await post.json();
   console.log(postsData);
+
+  postListEl.innerHTML = postsData.map(post => ` 
+    <div class="post">
+      <div class="post__title">
+        Post Title
+      </div>
+      <p class="post__body">
+        Post Body
+      </p>
+    </div>
+    `).join("");
 }
 
 main();
