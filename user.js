359ad217dcb0f4245ca3cde77b729aa0bedc5cc9
Raul Castillo <sx353553@yahoo.com>
2026-06-18 07:16:39 -0500

    update code

--- user.js
+++ user.js
@@ -1,10 +1,18 @@
 const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
   const id = event.target.value;
+  renderPost(id)
+}
+
+async function renderPost(id) {
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await posts.json();
-  postListEl.innerHTML = postsData.map(post => ` 
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join(""); 
+    
+  function postHTML(post) {
+   return ` 
     <div class="post">
       <div class="post__title">
        ${post.title}
@@ -13,25 +21,8 @@ async function onSearchChange(event) {
         ${post.body}
       </p>
     </div>
-    `).join("");
+    `
+  }
 }
 
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-  const postsData = await posts.json();
-  console.log(postsData);
-
-  postListEl.innerHTML = postsData.map(post => ` 
-    <div class="post">
-      <div class="post__title">
-       ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>
-    `).join("");
-}
-
-main();
+renderPost(id);
